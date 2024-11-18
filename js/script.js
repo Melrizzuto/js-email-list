2259b2b97b52eb4a3068d2f0cbdb5138528c9f91
Melrizzuto <melrizzuto4@gmail.com>
2024-11-18 14:14:00 +0100

    added three steps axios call js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 // STEP ONE: Faccio partire la chiamata come una stringa.
 
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail");
+axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+    //STEP TWO: se la request ha successo entra nel then con una funziona che riceve la risposta.
+
+    .then((res) => {
+        //STEP THREE: Stampa in console la mail casuale
+        console.log(res.data.response);
+
+    })
